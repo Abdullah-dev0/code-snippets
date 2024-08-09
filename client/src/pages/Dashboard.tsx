58d3ff26fd74c52422a077b0f2cac09bca3bace5
Abdullah-142 <abdulah14200@gmail.com>
2024-08-09 15:20:22 +0500

    Refactor project structure and remove unused files

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -2,6 +2,7 @@
 const Dashboard = () => {
 	return <div>
 		<h1 className="text-center">Dashboard</h1>
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab dolorum fugit impedit.</p>
 	</div>;
 };
 
