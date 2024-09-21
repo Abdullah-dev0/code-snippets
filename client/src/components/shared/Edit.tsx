497181317c9e96e5883d5fdd48d19f91abf6274f
Abdullah-142 <abdulah14200@gmail.com>
2024-09-22 02:03:26 +0500

    Remove unnecessary TypeScript build step and PostCSS configuration

--- client/src/components/shared/Edit.tsx
+++
@@ -1,8 +0,0 @@
-import { Snippet } from "@/types"; // Adjust the import according to your file structure
-import Snippetform from "./Snippetform";
-
-const Edit = ({ snippet }: { snippet: Snippet }) => {
-	return <Snippetform snippet={snippet} type="update" />;
-};
-
-export default Edit;
--- client/src/lib/snippetSchema/SnippetFom.tsx
+++
@@ -1,8 +0,0 @@
-import z from "zod";
-
-export const SnippetSchema = z.object({
-	title: z.string().min(1, "Tiltle Must Be At Least 3 Characters"),
-	language: z.string().min(1, "Please Select A Language"),
-	description: z.string().min(3, "Please Write A Description").max(200).trim(),
-	code: z.string().min(3, "Code Should not Be empty").max(1000).trim(),
-});
